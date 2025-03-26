6ddbcc9f21df1c325a5a73bc90abc333e081a9f8
Jay Dugad <jaydugad10@gmail.com>
2025-03-26 21:11:30 +0000

    Improve OMOP ID integration: add local cache, update README with licensing info

--- src/drug_named_entity_recognition/omop_api.py
+++ src/drug_named_entity_recognition/omop_api.py
@@ -1,18 +1,19 @@
 import requests
 
+# Local cache
 omop_cache = {}
 
 def get_rxcui(drug_name):
     if drug_name in omop_cache:
-        return omop_cache[drug_name]
+        return omop_cache[drug_name]  
 
     try:
         url = f"https://rxnav.nlm.nih.gov/REST/rxcui.json?name={drug_name}"
         response = requests.get(url)
         rxnorm_ids = response.json().get("idGroup", {}).get("rxnormId")
-        rxcui = rxnorm_ids[0] if rxnorm_ids else None
-        omop_cache[drug_name] = rxcui  #store in cache
-        return rxcui
+        omop_id = rxnorm_ids[0] if rxnorm_ids else None
+        omop_cache[drug_name] = omop_id  
+        return omop_id
     except Exception as e:
         print(f"Error fetching RxCUI for {drug_name}: {e}")
         return None
